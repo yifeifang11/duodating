59beb8dfbabb815a43cb63c97b60dec8d2d58035
Julia <julia.p.zhang@vanderbilt.edu>
2026-03-16 02:19:48 +0100

    Welcome Screen UI

--- app/src/androidTest/java/com/example/duodating/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/duodating/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.duodating
+package es.uc3m.duodating
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.duodating", appContext.packageName)
+        assertEquals("es.uc3m.duodating", appContext.packageName)
     }
 }
