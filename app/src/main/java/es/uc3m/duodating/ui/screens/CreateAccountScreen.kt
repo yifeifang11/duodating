b10829154ed7ca2fed98939e69a0b2f691e9194a
Julia <julia.p.zhang@vanderbilt.edu>
2026-03-16 14:18:40 +0100

    Create Account Screen UI

--- app/src/main/java/es/uc3m/duodating/ui/screens/CreateAccountScreen.kt
+++ app/src/main/java/es/uc3m/duodating/ui/screens/CreateAccountScreen.kt
@@ -1,22 +1,99 @@
 package es.uc3m.duodating.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.*
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+
 
 @Composable
 fun CreateAccountScreen(onAccountCreated: () -> Unit) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Create Account Screen")
-        Button(onClick = onAccountCreated) {
-            Text(text = "Sign Up")
+    val colorStops = arrayOf(
+        0.0f to MaterialTheme.colorScheme.background,
+        0.5f to MaterialTheme.colorScheme.secondary
+    )
+    Box(
+        modifier = Modifier
+            //.padding(horizontal = 40.dp)
+            .fillMaxSize()
+            .background(Brush.horizontalGradient(colorStops = colorStops)))
+    {
+        Column(
+            modifier = Modifier.fillMaxSize().padding(horizontal = 40.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Join the fun with your bestie", style = MaterialTheme.typography.displaySmall.copy(
+                color = MaterialTheme.colorScheme.onPrimary,
+                fontWeight = FontWeight.Bold
+                )
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Set up your account to start matching with other pairs and double your dating life",
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    color = MaterialTheme.colorScheme.onBackground,
+                    //textAlign = TextAlign.Center
+                ),
+                //modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            var text by remember {mutableStateOf("")}
+            OutlinedTextField(
+                value = text,
+                onValueChange = {text = it},
+                //state = rememberTextFieldState(initialText = "+1(555)000-0000"),
+                label = { Text("Phone Number")},
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedBorderColor = MaterialTheme.colorScheme.primary,
+                    unfocusedLabelColor = MaterialTheme.colorScheme.primary
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            var password by remember { mutableStateOf("") }
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                visualTransformation = PasswordVisualTransformation(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedBorderColor = MaterialTheme.colorScheme.primary,
+                    unfocusedLabelColor = MaterialTheme.colorScheme.primary
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = "By signing up, you agree to our Terms of Service and Privacy Policy.",
+                style = MaterialTheme.typography.bodyMedium.copy(
+                    color = MaterialTheme.colorScheme.onBackground,
+                    textAlign = TextAlign.Center
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(133.dp))
+
+            Button(onClick = onAccountCreated) {
+                Text(text = "Create Account")
+            }
         }
     }
+
 }
